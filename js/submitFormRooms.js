bb46ac9cc527d54759ba0a60dce543cf18493ea7
MatheusBeneti <matheusbeneti3@gmail.com>
2024-04-07 22:25:51 -0300

    adicionada funcionalidade para reservar quartos

--- js/submitFormRooms.js
+++ js/submitFormRooms.js
@@ -11,18 +11,15 @@ document.getElementById('bookingForm').addEventListener('submit', function(event
     var cafeDaManha = document.getElementById('cafeDaManha').checked;
 
     // Crie um objeto JSON com os valores
-    var data = {
+    const data = {
         clientName: clientName,
         clientCPF: clientCPF,
         inDate: inDate,
         outDate: outDate,
-        numberRoom: numberRoom,
+        numberRoom: parseInt(numberRoom), // Garanta que o número do quarto seja um número inteiro
         roomType: roomType,
         cafeDaManha: cafeDaManha
     };
-
-    // Converta o objeto JSON em uma string
-    var jsonData = JSON.stringify(data);
 
     // Defina a URL para enviar os dados
     var url = 'http://localhost:8080/rooms';
@@ -30,7 +27,7 @@ document.getElementById('bookingForm').addEventListener('submit', function(event
     // Faça uma solicitação POST usando Fetch API
     fetch(url, {
         method: 'POST',
-        body: jsonData,
+        body: JSON.stringify(data), // Converta o objeto para uma string JSON
         headers:{
             'Content-Type': 'application/json'
         }
